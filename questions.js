6b806fbcc18b0134b76d1a3e52e433c749792dac
Karlos6996 <127251448+Karlos6996@users.noreply.github.com>
2026-06-29 00:13:44 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -15,17 +15,37 @@ var QUESTIONS = [
 
   {
     q: "Why does google flag this site as “VIRUS“ ?",
-    a: "uses proper HTTPS."
+    a: "The website https://netflixkeys.com/ is a legitimate service that provides Netflix CDK activation keys for temporary Premium access. It does not perform phishing, steal user credentials, or contain malware - visitors only enter a purchased key and do not submit their Netflix login details. The Google Safe Browsing flag is a false positive triggered by the use of the Netflix brand name combined wit"
   },
 
   {
-    q: "Jak KarlosScript spustím?",
-    a: "Sem napiš postup, jak skript spustit."
+    q: "Account stopped working?",
+    a: "Please message me on eldorado! Make sure its in the warranty time which is 30days after purchase."
   },
 
   {
-    q: "Je to zdarma?",
-    a: "Sem napiš odpověď."
+    q: "Payment issue on the account?",
+    a: "Please message me on eldorado! Make sure its in the warranty time which is 30days after purchase."
+  },
+
+  {
+    q: "How do i set the PIN, I dont know the password!",
+    a: "To get the password from your account paste your CDK key to: https://netflixkeys.com/ and there you will find email+password from the Netflix."
+  },
+
+  {
+    q: "How do i log in on PC?",
+    a: "Paste here your CDK key which u got after purchase: https://netflixkeys.com/, after that select your platform (PC) and click open link."
+  },
+
+  {
+    q: "How do i log in on TV?",
+    a: "Paste here your CDK key which u got after purchase: https://netflixkeys.com/, after that select your platform (TV) and click open link, After that you should see space for code from your tv, go to your tv and select log-in with code, type the code from your TV to the link you have opened."
+  },
+
+  {
+    q: "Other issues?",
+    a: "Contact me on Eldorado!"
   }
 
 ];
